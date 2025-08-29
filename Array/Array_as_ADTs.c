243867e7d06033289838de8167fdd3ee9603e2eb
Subham Das <dassubham919@gmail.com>
2025-08-29 19:39:42 +0530

    Traversing of Array

--- Array/Array_as_ADTs.c
+++ Array/Array_as_ADTs.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 
-int main(){
-    
+void display(int arr[], int n){
+    for (int i=0; i<n; i++){
+        printf("%d ", arr[i]);
+    }
+}
+
+int main(){ 
+    int arr[]={1,2,6,78};
+    display(arr,4);
     return 0;
 }
