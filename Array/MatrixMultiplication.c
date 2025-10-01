2ac94e381819be6b7282cb6f129f5c4520e04e68
Subham Das <dassubham919@gmail.com>
2025-10-01 14:29:23 +0530

    Multiplication of matrix

---
+++ Array/MatrixMultiplication.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+
+int main(){
+    int A[10][10], B[10][10], M[10][10], i,j,k,m,n,p,q;
+
+    printf("Enter order of matrix A[10][10]: m and n: ");
+    scanf("%d %d", &m, &n);
+
+    printf("Enter order of matrix B[10][10]: p and q: ");
+    scanf("%d %d", &p, &q);
+    
+    if(n!=p){
+        printf("\nMatrix multiplication not possible\n");
+        return -1;
+    }
+
+    printf("Enter the elements of matrix A:");
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    printf("Enter the elements of matrix B:");
+    for(i=0; i<p; i++){
+        for(j=0; j<q; j++){
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    printf("Elements of matrix A:\n");
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            printf("%d ", A[i][j]);
+        }
+    }
+    printf("\n");
+    printf("Elements of matrix B:\n");
+    for(i=0; i<p; i++){
+        for(j=0; j<q; j++){
+            printf("%d ", B[i][j]);
+        }
+    }
+
+    printf("\n");
+    printf("Multiplication of Matrices A and B: \n");
+
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            M[i][j] = 0;
+            for(k=0; k<n; k++){
+                M[i][j] += A[i][k] + B[k][j]; 
+            }
+        }
+    }
+
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            printf("%d ", M[i][j]);
+        }
+    }
+    return 0;
+}
